4c009b8b8cea788365fd5e775a00ad81ac83bd10
dipak-95 <dipakladani95@gmail.com>
2025-12-13 00:15:55 +0530

    old code

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://amitedge-solution.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
 });
 
 // Add a request interceptor
