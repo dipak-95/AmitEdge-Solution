847d3900df6133d3ff27e66b0a80e2c37d8a9e93
dipak-95 <dipakladani95@gmail.com>
2025-12-13 00:06:53 +0530

    Api Update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://amitedge-solution.onrender.com/api',
 });
 
 // Add a request interceptor
